2113339ada5ae6276baa39bdde64923dba82364a
NguyenThu <nguyenthu753951@gmail.com>
2022-03-24 23:48:27 +0700

    Giao dien va DB

--- src/main/java/com/family/Family5Application.java
+++ src/main/java/com/family/Family5Application.java
@@ -2,8 +2,10 @@ package com.family;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.family.repository")
 public class Family5Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/family/controller/HomeController.java
+++ src/main/java/com/family/controller/HomeController.java
@@ -1,19 +1,27 @@
 package com.family.controller;
 
-import com.family.dto.ChiTietDonHang;
-import com.family.dto.Menu;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 
-import java.math.BigDecimal;
+import com.family.dto.Menu;
+import com.family.repository.MenuRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import java.util.List;
 
+
+@Controller
 public class HomeController {
 
+    @Autowired
+    MenuRepository menuRepository;
 
     @GetMapping("/")
-    public String index() {
-
-        return "index";
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        List<Menu> menuList = menuRepository.findAll();
+        modelAndView.addObject("menuList", menuList);
+        return modelAndView;
     }
 }
